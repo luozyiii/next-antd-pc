a28af3f52555d366dd2de751db17c3ed143a4a51
罗智毅 <924361501@qq.com>
2023-08-19 00:24:27 +0800

    fix: build 错误

--- src/app/demo/fetch/books/ApiDemo.tsx
+++ src/app/demo/fetch/books/ApiDemo.tsx
@@ -2,17 +2,26 @@ import Link from 'next/link';
 import { Card } from 'antd';
 import styles from './page.module.css';
 
+async function getData() {
+  try {
+    const res = await fetch('/api/book', {
+      cache: 'no-store',
+    });
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return res.json();
+  } catch (error) {}
+}
+
 export default async function ApiDemo() {
-  const res = await fetch('http://localhost:3000/api/book', {
-    cache: 'no-store',
-  });
-  const { data } = await res.json();
+  const res = await getData();
 
   return (
     <Card>
       <h6 className={styles.title}>【服务端渲染】API 路由转发第三方接口</h6>
       <ul className={styles.list}>
-        {data?.map((book: any, key: number) => {
+        {res?.data?.map((book: any, key: number) => {
           const _href = '/demo/fetch/books/' + String(book?.id);
           return (
             <li key={key}>
--- src/app/demo/fetch/books/page.tsx
+++ src/app/demo/fetch/books/page.tsx
@@ -7,6 +7,7 @@ export default function BooksPage() {
   return (
     <PageContent>
       <h4 style={{ marginBottom: '10px' }}>该页面演示了三种请求方式来获取列表数据。</h4>
+
       <FetchDemo />
       <br />
       <ApiDemo />
