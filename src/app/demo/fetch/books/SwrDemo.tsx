b46b3265d67b6c0770e63675b157af3add153342
罗智毅 <924361501@qq.com>
2023-08-23 23:35:31 +0800

    feat: react-content-loader骨架屏

--- src/app/demo/fetch/books/SwrDemo.tsx
+++ src/app/demo/fetch/books/SwrDemo.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import ContentLoader from 'react-content-loader';
 import Link from 'next/link';
 import useSWR from 'swr';
 import { Card } from 'antd';
@@ -11,7 +12,15 @@ function SwrDemo() {
   const { data, error, isLoading } = useSWR('/api/book', fetcher);
 
   if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
+  if (isLoading) {
+    return (
+      <ContentLoader viewBox="0 0 900 100">
+        <rect x="20" y="22" rx="0" ry="0" width="300" height="18" />
+        <rect x="20" y="48" rx="0" ry="0" width="100" height="12" />
+        <rect x="20" y="68" rx="0" ry="0" width="100" height="12" />
+      </ContentLoader>
+    );
+  }
 
   return (
     <Card>
